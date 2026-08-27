fd89f4962693f80f59f0a841a524690c69fb5ea9
jeondemo <jeondemo@gmail.com>
2026-08-27 17:28:33 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // 학생찾기 PWA Service Worker
-const CACHE_NAME = 'student-finder-v21-cdn';
+const CACHE_NAME = 'student-finder-v22-cdn';
 const STATIC_ASSETS = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
